5b4a12cc24b1cc1091d457fc311b9c91ba4d37f0
rayallen20 <452857163@qq.com>
2021-09-19 22:17:37 +0800

    第1章 并发概述 完成
    同步原语是啥不清楚
    并发结构是啥不清楚
    其余细节见笔记中的TODO

---
+++ code/chapter1/07-starvation/starvation.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	var sharedLock sync.Mutex
+	const runtime = 1 * time.Second
+
+	// 贪婪的goroutine
+	greedyWorker := func() {
+		defer wg.Done()
+
+		var count int
+
+		for begin := time.Now(); time.Since(begin) <= runtime; {
+			sharedLock.Lock()
+			time.Sleep(3 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			count++
+		}
+
+		fmt.Printf("Greedy worker was able to execute %v work loops\n", count)
+	}
+
+	// 平和的goroutine
+	politeWorker := func() {
+		defer wg.Done()
+
+		var count int
+
+		for begin := time.Now(); time.Since(begin) < runtime; {
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			count++
+		}
+
+		fmt.Printf("Polite worker was able to execute %v work loops\n", count)
+	}
+
+	wg.Add(2)
+	go greedyWorker()
+	go politeWorker()
+
+	wg.Wait()
+}
