13888a27e98586adc03d82704fedf90ea07da460
rayallen20c@imooc.com <452857163@qq.com>
2023-04-01 00:45:50 +0800

    3. 防止goroutine泄露 完成

---
+++ code/chapter4/05-cancelChildGroutine/cancelChildGroutine.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	doWork := func(done <-chan interface{}, stringStream <-chan string) <-chan interface{} {
+		terminated := make(chan interface{})
+		go func() {
+			defer fmt.Println("doWork exited")
+			defer close(terminated)
+			for {
+				select {
+				case s := <-stringStream:
+					// 模拟一些操作
+					fmt.Printf("Reveived: %s\n", s)
+				case <-done:
+					return
+				}
+			}
+		}()
+
+		return terminated
+	}
+
+	done := make(chan interface{})
+	terminated := doWork(done, nil)
+
+	go func() {
+		// 1s后取消goroutine的操作
+		time.Sleep(1 * time.Second)
+		fmt.Println("canceling doWork goroutine...")
+		close(done)
+	}()
+
+	// 在关闭前阻塞
+	<-terminated
+	fmt.Println("done")
+}
---
+++ code/chapter4/06-goroutineLeakBecauseWriteIntoChannelBlock/goroutineLeakBecauseWriteIntoChannelBlock.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	newRandStream := func() <-chan int {
+		randStream := make(chan int)
+		go func() {
+			defer fmt.Println("newRandStream closure exited.")
+			defer close(randStream)
+			for {
+				randStream <- rand.Int()
+			}
+		}()
+
+		return randStream
+	}
+
+	randStream := newRandStream()
+	fmt.Println("3 random integers:")
+
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("%d: %d\n", i, <-randStream)
+	}
+}
