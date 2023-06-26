e68d01431bbdbe5aff420ccfb4bfae9171f0dea7
yangLei <452857163@qq.com>
2023-06-26 18:15:01 +0800

    错误处理部分 复习完成

--- code/chapter4/11-sendErrorInGoroutine/sendErrorInGoroutine.go
+++ code/chapter4/11-sendErrorInGoroutine/sendErrorInGoroutine.go
@@ -11,27 +11,6 @@ type Result struct {
 }
 
 func main() {
-	checkStatus := func(done <-chan interface{}, urls ...string) <-chan Result {
-		results := make(chan Result)
-		go func() {
-			defer close(results)
-			for _, url := range urls {
-				resp, err := http.Get(url)
-				result := Result{
-					Error:    err,
-					Response: resp,
-				}
-				select {
-				case <-done:
-					return
-				case results <- result:
-				}
-			}
-		}()
-
-		return results
-	}
-
 	done := make(chan interface{})
 	defer close(done)
 	urls := []string{"https://www.baidu.com", "http://badHost"}
@@ -43,4 +22,25 @@ func main() {
 
 		fmt.Printf("Response: %v\n", result.Response.Status)
 	}
+}
+
+func checkStatus(done <-chan interface{}, urls ...string) <-chan Result {
+	results := make(chan Result)
+	go func() {
+		defer close(results)
+		for _, url := range urls {
+			resp, err := http.Get(url)
+			result := Result{
+				Error:    err,
+				Response: resp,
+			}
+			select {
+			case <-done:
+				return
+			case results <- result:
+			}
+		}
+	}()
+
+	return results
 }
--- code/chapter4/12-handleErrorInCaller/handleErrorInCaller.go
+++ code/chapter4/12-handleErrorInCaller/handleErrorInCaller.go
@@ -11,27 +11,6 @@ type Result struct {
 }
 
 func main() {
-	checkStatus := func(done <-chan interface{}, urls ...string) <-chan Result {
-		results := make(chan Result)
-		go func() {
-			defer close(results)
-			for _, url := range urls {
-				resp, err := http.Get(url)
-				result := Result{
-					Error:    err,
-					Response: resp,
-				}
-				select {
-				case <-done:
-					return
-				case results <- result:
-				}
-			}
-		}()
-
-		return results
-	}
-
 	done := make(chan interface{})
 	defer close(done)
 
@@ -50,4 +29,25 @@ func main() {
 
 		fmt.Printf("Response: %v\n", result.Response.Status)
 	}
+}
+
+func checkStatus(done <-chan interface{}, urls ...string) <-chan Result {
+	results := make(chan Result)
+	go func() {
+		defer close(results)
+		for _, url := range urls {
+			resp, err := http.Get(url)
+			result := Result{
+				Error:    err,
+				Response: resp,
+			}
+			select {
+			case <-done:
+				return
+			case results <- result:
+			}
+		}
+	}()
+
+	return results
 }
