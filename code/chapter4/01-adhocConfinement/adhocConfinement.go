eb91309d41244bb33e393b78636b95617ab5d7ff
yangLei <452857163@qq.com>
2023-06-26 09:23:54 +0800

    1. 更新了访问范围约束相关的代码和笔记 因为好久都没看了

--- code/chapter4/01-adhocConfinement/adhocConfinement.go
+++ code/chapter4/01-adhocConfinement/adhocConfinement.go
@@ -5,17 +5,17 @@ import "fmt"
 func main() {
 	data := make([]int, 4)
 
-	loopData := func(handleData chan<- int) {
-		defer close(handleData)
-		for i := range data {
-			handleData <- data[i]
-		}
-	}
-
 	handleData := make(chan int)
-	go loopData(handleData)
+	go loopData(handleData, data)
 
 	for num := range handleData {
 		fmt.Printf("%d\n", num)
+	}
+}
+
+func loopData(handleData chan<- int, data []int) {
+	defer close(handleData)
+	for i := range data {
+		handleData <- data[i]
 	}
 }
--- code/chapter4/03-confineNonConcurrentSafeData/confineNonConcurrentSafeData.go
+++ code/chapter4/03-confineNonConcurrentSafeData/confineNonConcurrentSafeData.go
@@ -7,19 +7,19 @@ import (
 )
 
 func main() {
-	printData := func(wg *sync.WaitGroup, data []byte) {
-		defer wg.Done()
-		var buffer bytes.Buffer
-		for _, byteData := range data {
-			fmt.Fprintf(&buffer, "%c", byteData)
-		}
-		fmt.Printf("%s\n", buffer.String())
-	}
-
 	var wg sync.WaitGroup
 	data := []byte("golang")
 	wg.Add(2)
 	go printData(&wg, data[:3])
 	go printData(&wg, data[3:])
 	wg.Wait()
+}
+
+func printData(wg *sync.WaitGroup, data []byte) {
+	defer wg.Done()
+	var buffer bytes.Buffer
+	for _, byteData := range data {
+		fmt.Fprintf(&buffer, "%c", byteData)
+	}
+	fmt.Printf("%s\n", buffer.String())
 }
