f7bf494426eb8d48b80ef959685f4f3b7ffde891
yangLei <452857163@qq.com>
2023-06-27 14:52:41 +0800

    or-done-channel 复习完成

--- code/chapter4/31-orDone/orDone.go
+++ code/chapter4/31-orDone/orDone.go
@@ -1,38 +1,41 @@
 package main
 
+import "fmt"
+
 func main() {
-	orDone := func(done <-chan interface{}, c <-chan interface{}) <-chan interface{} {
-		valueStream := make(chan interface{})
-
-		go func() {
-			defer close(valueStream)
-
-			for {
-				select {
-				case <-done:
-					return
-				case v, ok := <-c:
-					// 若给定的channel关闭 也直接返回
-					if !ok {
-						return
-					}
-
-					select {
-					case valueStream <- v:
-					case <-done:
-					}
-				}
-			}
-		}()
-
-		return valueStream
-	}
-
 	done := make(chan interface{})
 	defer close(done)
 	myChan := make(chan interface{})
 
 	for value := range orDone(done, myChan) {
 		// 使用从channel中读取到的值执行一些逻辑
+		fmt.Printf("%#v\n", value)
 	}
+}
+
+func orDone(done <-chan interface{}, c <-chan interface{}) <-chan interface{} {
+	valueStream := make(chan interface{})
+
+	go func() {
+		defer close(valueStream)
+
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-c:
+				// 若给定的channel关闭 也直接返回
+				if !ok {
+					return
+				}
+
+				select {
+				case valueStream <- v:
+				case <-done:
+				}
+			}
+		}
+	}()
+
+	return valueStream
 }
